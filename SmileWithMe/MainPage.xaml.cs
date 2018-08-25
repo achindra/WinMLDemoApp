7b86ec1bc6e0f3546c547f315539c66321bc79c1
Achindra Bhatnagar <achindra@live.com>
2018-08-25 23:42:06 +0530

    Sample Application to run ONNX model locally

--- SmileWithMe/MainPage.xaml.cs
+++ SmileWithMe/MainPage.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.AI.MachineLearning.Preview;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Media;
+using Windows.Storage.Streams;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,5 +32,47 @@ namespace SmileWithMe
         {
             this.InitializeComponent();
         }
+
+        private async void btnLoadPicture_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            lstEmotions.Items.Clear();
+            var filePicker = new Windows.Storage.Pickers.FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".jpg");
+            filePicker.FileTypeFilter.Add(".png");
+
+            var file = await filePicker.PickSingleFileAsync();
+            if (file == null)
+            {
+                //operation cancelled 
+                return;
+            }
+
+            BitmapImage imgBitmap = new BitmapImage(new Uri(file.Path));
+            imgPicture.Source = imgBitmap;
+
+            StorageFile modelFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri($"ms-appx:///Assets/Emotion.onnx"));
+            CNTKGraphModel model = await CNTKGraphModel.CreateCNTKGraphModel(modelFile);
+
+            SoftwareBitmap softwareBitmap;
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+                VideoFrame vf = VideoFrame.CreateWithSoftwareBitmap(softwareBitmap);
+                CNTKGraphModelInput input = new CNTKGraphModelInput();
+                input.Input338 = vf;
+
+                CNTKGraphModelOutput output = await model.EvaluateAsync(input);
+                foreach(var item in output.Plus692_Output_0)
+                {
+                    lstEmotions.Items.Add(item.ToString());
+                }
+            }
+
+
+            
+        }
+
     }
 }
